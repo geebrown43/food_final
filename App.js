41a605936a05572961ef6cefd3e971914918cbaf
Gavin Brown <scareb15@gmail.com>
2019-01-22 11:33:38 -0700

    updated for react native navigation

--- App.js
+++ App.js
@@ -1,44 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
+import configureStore from './src/store/createStore';
+import { registerScreens } from './src';
 
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-type Props = {
+const store = configureStore();
+registerScreens(store, Provider);
 
-};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+        root: {
+            stack: {
+                children: [{
+                    component: {
+                        name: 'app.Onboarding'
+                    }
+                }]
+            }
+        }
+    });
 });
